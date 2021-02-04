3feb147b19d405487b3b63f3605d1cff103fba79
Mandeep Dhillon <mandeep15@gmail.com>
2021-02-04 15:35:09 -0800

    Rendered bar button with successfully added functionality for adding new items via alert functions

--- ToDude/ToDude/itemListTableViewController.swift
+++ ToDude/ToDude/itemListTableViewController.swift
@@ -18,16 +18,49 @@ class itemListTableViewController: UITableViewController {
 
     }
 
-    // MARK: - Table view data source
-
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  
+  @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+    
+    let alertController = UIAlertController(
+      title: "Add a new item",
+      message: "",
+      preferredStyle: .alert
+      )
+    
+    var tempTextField = UITextField()
+    
+    let alertAction = UIAlertAction(title: "Done", style: .default) { (_) in
+      let newItem = Item(context: self.context)
+      if let text = tempTextField.text {
+        newItem.title = text
+        newItem.completed = false
+        
+        self.items.append(newItem)
+        
+        self.saveItems()
+      }
+    }
+    
+    alertController.addTextField { (textField) in
+      textField.placeholder = "To Do Title"
+      tempTextField = textField
+    }
+    
+    alertController.addAction(alertAction)
+    
+    present(alertController, animated: true, completion: nil)
+    
+  }
+ 
+  // MARK: - Table view data source
+   
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
       return items.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
 
         // Configure the cell...
       let item = items[indexPath.row]
